15264a6146d9aad39e393838e18e1d27d4a20094
viewshashank <shashankirctc58@gmail.com>
2026-02-26 20:01:20 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "For Harshita ❤️",
+  description: "A special surprise by Shashank",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
